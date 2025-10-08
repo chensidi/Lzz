b0a9d739e5e456c262cdad4f78cbbd94e07b20ad
Jacky_Cheung <2541012495@qq.com>
2025-10-08 17:12:09 +0800

    feat: linda

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -20,6 +20,11 @@ const playlist = [
     author: 'Jennie',
     file: 'https://github.com/chensidi/Jennie/blob/main/assets/musics/%E7%AA%81%E7%84%B6%E7%9A%84%E8%87%AA%E6%88%91%20(Live).mp3?raw=true',
   },
+   {
+    name: 'linda',
+    author: 'Jennie',
+    file: 'https://tx.stream.kg.qq.com/njc-kgsvp/1021_s_0bc35z7yqlifbqajrfovmzulv3wdrdualpsa.f0.m4a?vkey=47F16B0205577453B6A74D86896CB6001C4981CA734B66A91D48DE68A56C82B9C56616E4D7640F495A13B5B74582A0F645F81D7107DE541A9E989E7430C00457726196198DE8D4DAC6493453E7B458DCC96F3E08AED52BD0&dis_k=78a6680951eb0af22fecbc77c7b5f77e&dis_t=1759748966&fromtag=1021&ugcid=762983942_1758466275_562&nr=1',
+  },
 ]
 
 export default {
